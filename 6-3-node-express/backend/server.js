2e64733a81b8f0c27cbc7a718d82807002f8ff5a
Ali-Kfupm <s202322750@kfupm.edu.sa>
2026-04-06 10:11:16 +0300

    Complete TODO 1

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,7 +1,10 @@
 // TODO 1: Import and Create express app instance
+import express from "express";
+import { getRandomQuote } from "./quotes.js";
 
+const app = express();
 // TODO 1: Define server port
-
+const PORT = 3000;
 
 // TODO 4: Import and Apply CORS middleware
 
